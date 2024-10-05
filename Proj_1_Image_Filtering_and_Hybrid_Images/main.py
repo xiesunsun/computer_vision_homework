0c3021c1324de09ea3a3d27a02f63f02e5e9b369
xiesunsun <3235441500@qq.com>
2024-10-05 21:25:08 +0800

    初步完成了第一个混合图片的人物

--- Proj_1_Image_Filtering_and_Hybrid_Images/main.py
+++ Proj_1_Image_Filtering_and_Hybrid_Images/main.py
@@ -1,5 +1,31 @@
 import cv2
-import numpy
+import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
+#读取图像
+image = cv2.imread('./pic_data/image1.jpg')
 
-imgae = cv2.imread()
+#提取低频信息
+low_frequencies=cv2.GaussianBlur(image,(31,31),0)
+
+print("我正在运行！")
+#提取高频信息
+high_frequencies=image-low_frequencies
+
+ 
+# plt.switch_backend('agg')
+#显示结果
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.imshow(low_frequencies,cmap="gray")
+plt.title("low frequencies")
+
+plt.subplot(1,2,2)
+plt.imshow(high_frequencies,cmap="gray")
+plt.title("High frequencies")
+# 远程服务器无法进行图形显示
+
+# plt.show()
+
+#把相应的照片保存下来
+plt.savefig("./res_data/1.jpg")
